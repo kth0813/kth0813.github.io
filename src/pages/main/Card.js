96082a0c0cac221791736f4f1f2dc5dd6234687c
kth0813 <ksskth0813@naver.com>
2024-01-09 12:11:39 +0900

    aaa

--- src/pages/main/Card.js
+++ src/pages/main/Card.js
@@ -1,116 +1,117 @@
 import React, { useState, useEffect, Fragment } from "react";
 import "../../App.css";
 import Roulette from "./Roulette";
+
 export default function Card() {
   const [cards, setCards] = useState([]);
   const [mainCard, setMainCard] = useState({
     id: 999,
-    text: "",
+    img: "",
     flipped: false,
   });
   const [showRoulette, setShowRoulette] = useState(false);
   const [centered, setCentered] = useState(false);
-  const allTexts = [
-    "Text1",
-    "Text2",
-    "Text3",
-    "Text4",
-    "Text5",
-    "Text6",
-    "Text7",
-    "Text8",
-    "Text9",
-    "Text10",
-    "Text11",
-    "Text12",
-    "Text13",
-    "Text14",
-    "Text15",
-    "Text16",
-    "Text17",
-    "Text18",
-    "Text19",
-    "Text20",
-    "Text21",
-    "Text22",
-    "Text23",
-    "Text24",
-    "Text25",
-    "Text26",
-    "Text27",
-    "Text28",
-    "Text29",
-    "Text30",
-    "Text31",
-    "Text32",
-    "Text33",
-    "Text34",
-    "Text35",
-    "Text36",
-    "Text37",
-    "Text38",
-    "Text39",
-    "Text40",
-    "Text41",
-    "Text42",
-    "Text43",
-    "Text44",
-    "Text45",
-    "Text46",
-    "Text47",
-    "Text48",
-    "Text49",
-    "Text50",
+  const allImg = [
+    "card01",
+    "card02",
+    "card03",
+    "card04",
+    "card05",
+    "card06",
+    "card07",
+    "card08",
+    "card09",
+    "card10",
+    "card11",
+    "card12",
+    "card13",
+    "card14",
+    "card15",
+    "card16",
+    "card17",
+    "card18",
+    "card19",
+    "card20",
+    "card21",
+    "card22",
+    "card23",
+    "card24",
+    "card25",
+    "card26",
+    "card27",
+    "card28",
+    "card29",
+    "card30",
+    "card31",
+    "card32",
+    "card33",
+    "card34",
+    "card35",
+    "card36",
+    "card37",
+    "card38",
+    "card39",
+    "card40",
+    "card41",
+    "card42",
+    "card43",
+    "card44",
+    "card45",
+    "card46",
+    "card47",
+    "card48",
+    "card49",
+    "card50",
+    "card51",
+    "card52",
   ];
+
   const allMembers = [
-    { name: "member1", use: "Y" },
-    { name: "member2", use: "N" },
-    { name: "member3", use: "Y" },
-    { name: "member4", use: "N" },
-    { name: "member5", use: "N" },
-    { name: "member6", use: "N" },
-    { name: "member7", use: "N" },
-    { name: "member8", use: "N" },
-    { name: "member9", use: "N" },
-    { name: "member10", use: "N" },
-    { name: "member11", use: "N" },
-    { name: "member12", use: "N" },
-    { name: "member13", use: "N" },
-    { name: "member14", use: "N" },
-    { name: "member15", use: "N" },
-    { name: "member16", use: "N" },
-    { name: "member17", use: "N" },
-    { name: "member18", use: "N" },
-    { name: "member19", use: "N" },
-    { name: "member20", use: "N" },
-    { name: "member21", use: "N" },
-    { name: "member22", use: "N" },
-    { name: "member23", use: "N" },
-    { name: "member24", use: "N" },
-    { name: "member25", use: "N" },
-    { name: "member26", use: "N" },
-    { name: "member27", use: "N" },
-    { name: "member28", use: "N" },
-    { name: "member29", use: "N" },
-    { name: "member30", use: "N" },
+    { name: "김예림", use: "N" },
+    { name: "백민우", use: "N" },
+    { name: "이하빈", use: "N" },
+    { name: "장진영", use: "N" },
+    { name: "전솔담", use: "N" },
+    { name: "정민성", use: "N" },
+    { name: "정회창", use: "N" },
+    { name: "김여명", use: "N" },
+    { name: "김태훈", use: "N" },
+    { name: "이나래", use: "N" },
+    { name: "이유나", use: "N" },
+    { name: "주예지", use: "N" },
+    { name: "김지원", use: "N" },
+    { name: "박도희", use: "N" },
+    { name: "백민서", use: "N" },
+    { name: "이민수", use: "N" },
+    { name: "이연우", use: "N" },
+    { name: "차영광", use: "N" },
+    { name: "김예송", use: "Y" },
+    { name: "이명철", use: "N" },
+    { name: "김대원", use: "N" },
+    { name: "박병호", use: "N" },
+    { name: "이진호", use: "N" },
+    { name: "김준혁", use: "N" },
+    { name: "이찬미", use: "N" },
+    { name: "김철 목사님", use: "N" },
+    { name: "박순성 집사님", use: "N" },
   ];
   useEffect(() => shuffleCard(), []);
   const shuffleCard = () => {
-    const texts = generateRandomTexts();
-    const initialCards = texts.reduce((accumulator, text) => {
-      return [...accumulator, { id: accumulator.length, text, flipped: false }];
+    const texts = generateRandomImg();
+    const initialCards = texts.reduce((accumulator, img) => {
+      return [...accumulator, { id: accumulator.length, img, flipped: false }];
     }, []);
     const shuffledCards = initialCards.sort(() => Math.random() - 0.5);
     const selectedCards = shuffledCards.slice(0, 8);
     setCards(selectedCards);
   };
-  const generateRandomTexts = () => {
+  const generateRandomImg = () => {
     const shuffledTexts = [];
     const selectedTexts = new Set();
 
     while (shuffledTexts.length < 8) {
-      const randomIndex = Math.floor(Math.random() * allTexts.length);
-      const newText = allTexts[randomIndex];
+      const randomIndex = Math.floor(Math.random() * allImg.length);
+      const newText = allImg[randomIndex];
       if (!selectedTexts.has(newText)) {
         selectedTexts.add(newText);
         shuffledTexts.push(newText);
@@ -159,28 +160,93 @@ export default function Card() {
   return (
     <div>
       <div className="button-container">
-        <button onClick={centerCards}>카드 섞기</button>{" "}
-        <button onClick={openAllCards}>모두 보기</button>{" "}
-        <button onClick={choice}>
+        <button
+          onClick={choice}
+          style={{
+            width: "100px",
+            height: "100px",
+            borderRadius: "50%",
+            backgroundColor: "#3498db",
+            color: "#ffffff",
+            fontSize: "16px",
+            cursor: "pointer",
+            margin: "10px 50px",
+          }}
+        >
           {showRoulette ? "카드 뽑기" : "사람 뽑기"}
         </button>
+        <br />
+        {!showRoulette && (
+          <Fragment>
+            <button
+              onClick={centerCards}
+              style={{
+                width: "100px",
+                height: "100px",
+                borderRadius: "50%",
+                backgroundColor: "#3498db",
+                color: "#ffffff",
+                fontSize: "16px",
+                cursor: "pointer",
+                margin: "10px 50px",
+              }}
+            >
+              카드 섞기
+            </button>
+            <br />
+            <button
+              onClick={openAllCards}
+              style={{
+                width: "100px",
+                height: "100px",
+                borderRadius: "50%",
+                backgroundColor: "#3498db",
+                color: "#ffffff",
+                fontSize: "16px",
+                cursor: "pointer",
+                margin: "10px 50px",
+              }}
+            >
+              모두 보기
+            </button>
+          </Fragment>
+        )}
       </div>
-      <div className="member-list" style={{ overflow: "scroll" }}>
-        {allMembers
-          .sort((a, b) => a.use.localeCompare(b.use))
-          .map((member, i) => (
-            <div key={i} className={member.use == "Y" ? "used" : ""}>
-              {member.name}
-            </div>
-          ))}
-      </div>
+      {showRoulette && (
+        <Fragment>
+          <div style={{ fontSize: "20px" }}>다음 지목될 사람 후보</div>
+          <div
+            className="member-list"
+            style={{ overflow: "scroll", border: "2px solid" }}
+          >
+            {allMembers
+              .sort((a, b) => a.use.localeCompare(b.use))
+              .map((member, i) => (
+                <div
+                  key={i}
+                  className={member.use == "Y" ? "used" : ""}
+                  style={{ margin: "5px" }}
+                >
+                  {member.name}
+                </div>
+              ))}
+          </div>
+        </Fragment>
+      )}
       {showRoulette ? (
         <div style={{ left: "40%", top: "10%", position: "absolute" }}>
           <Roulette
             member={allMembers
               .filter((member) => member.use == "N")
               .map((member) => {
-                return { ...member, option: member.name, percentage: 100 };
+                return {
+                  ...member,
+                  option: member.name,
+                  percentage: Math.ceil(
+                    100 /
+                      allMembers.filter((member) => member.use == "N").length
+                  ),
+                };
               })}
           />
         </div>
@@ -206,18 +272,19 @@ export default function Card() {
             >
               <div className="card-inner">
                 <div className="card-front"></div>
-                <div className="card-back">{card.text}</div>
+                <div className="card-back">
+                  <div className={"wh " + card.img} />
+                </div>
               </div>
             </div>
           ))}
         </div>
       )}
-
       {mainCard.flipped && (
         <Fragment>
           <div className="mainCard">
             <div className="card-inner">
-              <div className="">{mainCard.text}</div>
+              <div className={"wh " + mainCard.img} />
             </div>
           </div>
           <div
--- src/pages/main/Roulette.js
+++ src/pages/main/Roulette.js
@@ -7,8 +7,9 @@ export default function Roulette(props) {
 
   const [mustSpin, setMustSpin] = useState(false); //룰렛이 회전 애니메이션을 시작
   const [prizeNumber, setPrizeNumber] = useState(0); //당첨 인덱스
-
+  const [prize, setPrize] = useState("");
   const handleSpinClick = () => {
+    setPrize("");
     if (!mustSpin) {
       // 1. 랜덤 기준점 설정
       const pivot = Math.floor(Math.random() * 99 + 1);
@@ -39,11 +40,26 @@ export default function Roulette(props) {
 
   const StopSpinning = () => {
     setMustSpin(false);
-    alert(data[prizeNumber].option + "이 당첨되셨습니다");
+    setPrize(data[prizeNumber].option);
   };
 
   return (
     <div className="App">
+      <button
+        onClick={handleSpinClick}
+        style={{
+          zIndex: 1,
+          width: "100px",
+          height: "100px",
+          borderRadius: "50%",
+          backgroundColor: "#3498db",
+          color: "#ffffff",
+          fontSize: "20px",
+          cursor: "pointer",
+        }}
+      >
+        시작
+      </button>
       <Wheel
         spinDuration={1}
         startingOptionIndex={Math.floor(Math.random() * data.length)}
@@ -52,10 +68,7 @@ export default function Roulette(props) {
         data={data}
         onStopSpinning={StopSpinning}
       />
-      <button onClick={handleSpinClick} style={{ zIndex: 1 }}>
-        SPIN
-      </button>
-      <div>{prizeNumber}</div>
+      {prize && <div style={{ fontSize: "40px" }}>당첨자 : {prize}</div>}
     </div>
   );
 }
