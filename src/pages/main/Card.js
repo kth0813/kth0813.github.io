148ed46b1ebb73e45d0bdadfe22c565d479d61f7
kth0813 <ksskth0813@naver.com>
2025-01-21 13:51:55 +0900

    g

--- src/pages/main/Card.js
+++ src/pages/main/Card.js
@@ -6,69 +6,19 @@ import Map from "./Map";
 
 export default function Card() {
   const [cards, setCards] = useState([]);
-  const [mainCard, setMainCard] = useState({
-    id: 999,
-    img: "",
-    flipped: false,
-  });
+  const [mainCard, setMainCard] = useState({ id: 999, img: "", flipped: false });
   const [showRoulette, setShowRoulette] = useState(false);
   const [showRoulette2, setShowRoulette2] = useState(false);
   const [showMap, setShowMap] = useState(false);
   const [centered, setCentered] = useState(true);
   const [start, setStart] = useState(false);
   const allImg = [
-    "card01",
-    "card02",
-    "card03",
-    "card04",
-    "card05",
-    "card06",
-    "card07",
-    "card08",
-    "card09",
-    "card10",
-    "card11",
-    "card12",
-    "card13",
-    "card14",
-    "card15",
-    "card16",
-    "card17",
-    "card18",
-    "card19",
-    "card20",
-    "card21",
-    "card22",
-    "card23",
-    "card24",
-    "card25",
-    "card26",
-    "card27",
-    "card28",
-    "card29",
-    "card30",
-    "card31",
-    "card32",
-    "card33",
-    "card34",
-    "card35",
-    "card36",
-    "card37",
-    "card38",
-    "card39",
-    "card40",
-    "card41",
-    "card42",
-    "card43",
-    "card44",
-    "card45",
-    "card46",
-    "card47",
-    "card48",
-    "card49",
-    "card50",
-    "card51",
-    "card52",
+    ["card01", "card02", "card03", "card04", "card05", "card06", "card07", "card08", "card09"],
+    ["card10", "card11", "card12", "card13", "card14", "card15", "card16", "card17", "card18"],
+    ["card19", "card20", "card21", "card22", "card23", "card24", "card25", "card26", "card27"],
+    ["card28", "card29", "card30", "card31", "card32", "card33", "card34", "card35", "card36"],
+    ["card37", "card38", "card39", "card40", "card41", "card42", "card43", "card44", "card45"],
+    ["card46", "card47", "card48", "card49", "card50", "card51", "card52"]
   ];
 
   const allMembers = [
@@ -103,7 +53,7 @@ export default function Card() {
     { name: "이민수", use: "Y" },
     { name: "이신우", use: "N" },
     { name: "이연우", use: "Y" },
-    { name: "이유나", use: "N" },
+    { name: "이유나", use: "Y" },
     { name: "이지우", use: "Y" },
     { name: "이진호", use: "Y" },
     { name: "이찬미", use: "Y" },
@@ -116,14 +66,14 @@ export default function Card() {
     { name: "최소민", use: "N" },
     { name: "최혜원", use: "Y" },
     { name: "한예지", use: "Y" },
-    { name: "김주훈 강도사님", use: "N" },
-    { name: "박순성 집사님", use: "Y" },
+    { name: "김주훈 강도사님", use: "Y" },
+    { name: "박순성 집사님", use: "Y" }
   ];
   useEffect(() => shuffleCard(), []);
   const shuffleCard = () => {
     const texts = generateRandomImg();
-    const initialCards = texts.reduce((accumulator, img) => {
-      return [...accumulator, { id: accumulator.length, img, flipped: false }];
+    const initialCards = texts.reduce((acc, img) => {
+      return [...acc, { id: acc.length, img, flipped: false }];
     }, []);
     const shuffledCards = initialCards.sort(() => Math.random() - 0.5);
     const selectedCards = shuffledCards.slice(0, 8);
@@ -132,10 +82,8 @@ export default function Card() {
   const generateRandomImg = () => {
     const shuffledTexts = [];
     const selectedTexts = new Set();
-
     while (shuffledTexts.length < 8) {
-      const randomIndex = Math.floor(Math.random() * allImg.length);
-      const newText = allImg[randomIndex];
+      const newText = allImg.flat()[Math.floor(Math.random() * allImg.flat().length)];
       if (!selectedTexts.has(newText)) {
         selectedTexts.add(newText);
         shuffledTexts.push(newText);
@@ -146,24 +94,17 @@ export default function Card() {
   };
 
   const ClickCard = (id) => {
-    setCards(
-      cards.map((card) =>
-        card.id == id ? { ...card, flipped: !card.flipped } : card
-      )
-    );
-    setMainCard({
-      ...cards.find((card) => card.id == id),
-      flipped: !cards.find((card) => card.id == id).flipped,
+    setCards(cards.map((card) => (card.id == id ? { ...card, flipped: !card.flipped } : card)));
+    setMainCard((prev) => {
+      const card = cards.find((card) => card.id == id);
+      if (!card) return prev;
+      return { ...card, flipped: !card.flipped };
     });
   };
 
   const centerCards = async () => {
     const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-    setCards(
-      cards.map((card) => {
-        return { ...card, flipped: false };
-      })
-    );
+    setCards((prev) => prev.map((card) => ({ ...card, flipped: false })));
     await delay(300);
     setCentered(true);
     await delay(300);
@@ -172,24 +113,8 @@ export default function Card() {
     setCentered(false);
   };
 
-  const openAllCards = () => {
-    setCards(
-      cards.map((card) => {
-        return { ...card, flipped: true };
-      })
-    );
-  };
-
-  const number = (num) => {
-    if (num == 1) return "4";
-    if (num == 2) return "8";
-    if (num == 3) return "3";
-    if (num == 4) return "7";
-    if (num == 5) return "2";
-    if (num == 6) return "6";
-    if (num == 7) return "1";
-    if (num == 8) return "5";
-  };
+  const openAllCards = () => setCards((prev) => prev.map((card) => ({ ...card, flipped: true })));
+  const number = (num) => ["4", "8", "3", "7", "2", "6", "1", "5"][num - 1];
   const choice = () => setShowRoulette(!showRoulette);
 
   return (
@@ -235,22 +160,8 @@ export default function Card() {
           </button>
           {showRoulette && (
             <Fragment>
-              <div
-                style={{
-                  fontSize: "20px",
-                  textAlign: "center",
-                }}
-              >
-                다음 지목될 사람 후보
-              </div>
-              <div
-                className="member-list"
-                style={{
-                  overflow: "scroll",
-                  border: "2px solid",
-                  borderRadius: "10px",
-                }}
-              >
+              <div style={{ fontSize: "20px", textAlign: "center" }}>다음 지목될 사람 후보</div>
+              <div className="member-list" style={{ overflow: "scroll", border: "2px solid", borderRadius: "10px" }}>
                 {allMembers
                   .sort((a, b) => a.use.localeCompare(b.use))
                   .map((member, i) => (
@@ -280,10 +191,7 @@ export default function Card() {
                 return {
                   ...member,
                   option: member.name,
-                  percentage: Math.ceil(
-                    100 /
-                      allMembers.filter((member) => member.use != "Y").length
-                  ),
+                  percentage: Math.ceil(100 / allMembers.filter((member) => member.use != "Y").length)
                 };
               })}
           />
@@ -300,13 +208,9 @@ export default function Card() {
               className={`card${card.flipped ? " flipped" : ""}`}
               onClick={() => ClickCard(card.id)}
               style={{
-                top: centered
-                  ? "calc(40% + 50px)"
-                  : `calc(${(index % 2) * 50}% + 50px)`,
-                right: centered
-                  ? "calc(20% + 50px)"
-                  : `calc(${Math.floor(index / 2) * 20}% + 50px)`,
-                transform: centered ? "translate(-50%, -50%)" : "none",
+                top: centered ? "calc(40% + 50px)" : `calc(${(index % 2) * 50}% + 50px)`,
+                right: centered ? "calc(20% + 50px)" : `calc(${Math.floor(index / 2) * 20}% + 50px)`,
+                transform: centered ? "translate(-50%, -50%)" : "none"
               }}
             >
               <div className="card-inner">
@@ -321,7 +225,7 @@ export default function Card() {
                       margin: 0,
                       textAlign: "center",
                       borderRadius: "15px",
-                      fontWeight: "bold",
+                      fontWeight: "bold"
                     }}
                   >
                     {number(index + 1)}
@@ -330,12 +234,7 @@ export default function Card() {
                 <div className="card-back">
                   <div
                     className={card.img}
-                    style={{
-                      borderRadius: "10px",
-                      width: "100%",
-                      height: 0,
-                      paddingBottom: "145.8%",
-                    }}
+                    style={{ borderRadius: "10px", width: "100%", height: 0, paddingBottom: "145.8%" }}
                   />
                 </div>
               </div>
@@ -344,14 +243,7 @@ export default function Card() {
         </div>
       ) : (
         <Fragment>
-          <div
-            style={{
-              zIndex: 100,
-              top: "50%",
-              right: "50%",
-              position: "absolute",
-            }}
-          >
+          <div style={{ zIndex: 100, top: "50%", right: "50%", position: "absolute" }}>
             <button
               onClick={() => {
                 setStart(true);
@@ -371,21 +263,11 @@ export default function Card() {
             <div className="card-inner">
               <div
                 className={mainCard.img}
-                style={{
-                  borderRadius: "20px",
-                  width: "100%",
-                  height: 0,
-                  paddingBottom: "145.8%",
-                }}
+                style={{ borderRadius: "20px", width: "100%", height: 0, paddingBottom: "145.8%" }}
               />
             </div>
           </div>
-          <div
-            className="bg"
-            onClick={() => {
-              setMainCard({ ...mainCard, flipped: false });
-            }}
-          />
+          <div className="bg" onClick={() => setMainCard({ ...mainCard, flipped: false })} />
         </Fragment>
       )}
     </div>
--- src/pages/main/Roulette.js
+++ src/pages/main/Roulette.js
@@ -2,9 +2,7 @@ import { Wheel } from "react-custom-roulette";
 import { Fragment, useState } from "react";
 
 export default function Roulette(props) {
-  //데이터
   const data = props.member;
-
   const [mustSpin, setMustSpin] = useState(false); //룰렛이 회전 애니메이션을 시작
   const [prizeNumber, setPrizeNumber] = useState(0); //당첨 인덱스
   const [prize, setPrize] = useState({});
@@ -15,7 +13,7 @@ export default function Roulette(props) {
       const pivot = Math.floor(Math.random() * 99 + 1);
       let stack = 0; // 가중치
 
-      let percentage = data.map((row, idx) => {
+      let percentage = data.map((row) => {
         {
           return row.percentage;
         }
@@ -24,8 +22,7 @@ export default function Roulette(props) {
       let newPrizeNumber = null; //당첨 인덱스
 
       percentage.some((row, idx) => {
-        //2. 가중치 누적
-        stack += row;
+        stack += row; //2. 가중치 누적
 
         // 3. 누적 가중치 값이 기준점 이상이면 종료
         if (pivot <= stack) {
@@ -59,7 +56,7 @@ export default function Roulette(props) {
           boxShadow: "3px 3px 10px rgba(0, 0, 0, 0.5)",
           border: "none",
           fontWeight: "bold",
-          marginBottom: "30px",
+          marginBottom: "30px"
         }}
       >
         돌리기
@@ -73,15 +70,7 @@ export default function Roulette(props) {
         data={data}
         onStopSpinning={StopSpinning}
         textColors={["#3E3E3E"]}
-        backgroundColors={[
-          "#FFD8D8",
-          "#FAE0D4",
-          "#FAF4C0",
-          "#CEFBC9",
-          "#D9E5FF",
-          "#E8D9FF",
-          "#FFD9EC",
-        ]}
+        backgroundColors={["#FFD8D8", "#FAE0D4", "#FAF4C0", "#CEFBC9", "#D9E5FF", "#E8D9FF", "#FFD9EC"]}
         outerBorderColor="gray"
         outerBorderWidth="1"
         radiusLineColor="gray"
@@ -92,8 +81,7 @@ export default function Roulette(props) {
         <Fragment>
           <div style={{ fontSize: "40px" }}>당첨자 : {prize.option} !!!</div>
           <div style={{ fontSize: "25px" }}>
-            금요일까지 예지총무님에게 <br />
-            답변을 보내주세요 !
+            금요일까지 윤걸총무님에게 <br /> 답변을 보내주세요 !
           </div>
         </Fragment>
       )}
