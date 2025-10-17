d54be4ccadf0576fadd117c9137daa1d12810bcd
kth0813 <staehun081@aprocni.com>
2025-10-17 11:50:10 +0900

    f

--- src/pages/Card.jsx
+++ src/pages/Card.jsx
@@ -1,17 +1,16 @@
 import React, { useState, useEffect, Fragment } from "react";
-
+const allImg = [
+  ["card01", "card02", "card03", "card04", "card05", "card06", "card07", "card08", "card09"],
+  ["card10", "card11", "card12", "card13", "card14", "card15", "card16", "card17", "card18"],
+  ["card19", "card20", "card21", "card22", "card23", "card24", "card25", "card26", "card27"],
+  ["card28", "card29", "card30", "card31", "card32", "card33", "card34", "card35", "card36"],
+  ["card37", "card38", "card39", "card40", "card41", "card42", "card43", "card44", "card45"],
+  ["card46", "card47", "card48", "card49", "card50", "card51", "card52"]
+];
 export default function Card() {
   const [cards, setCards] = useState([]);
   const [mainCard, setMainCard] = useState({ id: 999, img: "", flipped: false });
   const [centered, setCentered] = useState(true);
-  const allImg = [
-    ["card01", "card02", "card03", "card04", "card05", "card06", "card07", "card08", "card09"],
-    ["card10", "card11", "card12", "card13", "card14", "card15", "card16", "card17", "card18"],
-    ["card19", "card20", "card21", "card22", "card23", "card24", "card25", "card26", "card27"],
-    ["card28", "card29", "card30", "card31", "card32", "card33", "card34", "card35", "card36"],
-    ["card37", "card38", "card39", "card40", "card41", "card42", "card43", "card44", "card45"],
-    ["card46", "card47", "card48", "card49", "card50", "card51", "card52"]
-  ];
 
   useEffect(() => {
     const generateRandomImg = () => {
