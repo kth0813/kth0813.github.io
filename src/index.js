589ab7def586fcf626fb08fa7455652d594a8a09
kth0813 <staehun081@aprocni.com>
2026-02-19 17:01:30 +0900

    gg

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from "react-dom/client";
 import "./styles.css?20260109";
 import "./img/fontawesome-free-6.1.1-web/css/all.css";
 import App from "./App";
-import AuthProvider from "./AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <App />
   </React.StrictMode>
 );
--- src/pages/Left.jsx
+++ src/pages/Left.jsx
@@ -1,23 +1,6 @@
-import { NavLink /* useNavigate*/ } from "react-router-dom";
-// import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
-// import { useEffect, useState } from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Left({ onClickLink }) {
-  // const [user, setUser] = useState(null);
-  // const navigate = useNavigate();
-  // const auth = getAuth();
-
-  // useEffect(() => {
-  //   const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
-  //     setUser(currentUser);
-  //   });
-  //   return () => unsubscribe();
-  // }, [auth]);
-
-  // const handleLogout = async () => {
-  //   await signOut(auth);
-  //   navigate("/main");
-  // };
   return (
     <aside className="left-aside">
       <h2 className="left-title">메뉴</h2>
@@ -50,26 +33,6 @@ export default function Left({ onClickLink }) {
               룰렛 추첨
             </NavLink>
           </li>
-          {/* <li className="left-menu-item">
-            <Link to="/board" className="left-link" onClick={onClickLink}>
-              게시판
-            </Link>
-          </li>
-           <li className="left-menu-item">
-            {user ? (
-              <div
-                className="left-link"
-                onClick={handleLogout}
-                style={{ background: "none", border: "none", padding: 0, cursor: "pointer" }}
-              >
-                로그아웃
-              </div>
-            ) : (
-              <Link to="/login" className="left-link" onClick={onClickLink}>
-                로그인
-              </Link>
-            )}
-          </li> */}
         </ul>
       </nav>
     </aside>
